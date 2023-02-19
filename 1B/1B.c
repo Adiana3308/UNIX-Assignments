29320629507a237b4d82020cf7e2802f81e2a518
Aditi Ghate <107868821+Adiana3308@users.noreply.github.com>
2023-02-19 16:50:36 +0530

    Add files via upload

---
+++ 1B/1B.c
@@ -0,0 +1,19 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<unistd.h>
+//int main()
+//{
+//char *args[]={"./EXEC",NULL};
+//execvp(args[0],args);
+//printf("Ending\n");
+//return 0;
+//}
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+int main()
+{
+printf("I am EXEC.c called by execvp()\n");
+return 0;
+}
